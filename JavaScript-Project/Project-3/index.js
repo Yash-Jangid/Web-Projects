3930e864412b030e9c501918b2f0f18406140fa1
USeit-911 <yash.jangid.2019@ecajmer.ac.in>
2023-08-04 17:03:17 +0530

    final chnage P.3

---
+++ JavaScript-Project/Project-3/index.js
@@ -0,0 +1,40 @@
+function adjustJumbotronHeight() {
+    $(".jumbotron").css({ height: $(window).height() + "px" });
+  }
+
+  $(document).ready(function() {
+    adjustJumbotronHeight();
+
+    $(window).on("resize", function() {
+      adjustJumbotronHeight();
+    });
+  });
+
+  document.addEventListener("DOMContentLoaded", function () {
+    const flipContainers = document.querySelectorAll(".flip-container");
+
+    flipContainers.forEach(container => {
+        container.addEventListener("click", () => {
+            container.classList.toggle("hover");
+        });
+    });
+});
+$(function () {
+    $("#carousel-multiple").on("slide.bs.carousel", function (e) {
+      var itemsPerSlide = parseInt($(this).attr('data-maximum-items-per-slide')),
+        totalItems = $(".carousel-item", this).length,
+        reserve = 1,//do not change
+        $itemsContainer = $(".carousel-inner", this),
+        it = (itemsPerSlide + reserve) - (totalItems - e.to);
+  
+      if (it > 0) {
+        for (var i = 0; i < it; i++) {
+          $(".carousel-item", this)
+            .eq(e.direction == "left" ? i : 0)
+            // append slides to the end/beginning
+            .appendTo($itemsContainer);
+        }
+      }
+    });
+  });
+  
